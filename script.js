10fa53cc8cdd52d439835d474d5455930565140c
spyrosgew <spyrosgewdark@gmail.com>
2024-05-07 00:51:23 +0300

    finished html css

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // Function to be executed when the div is clicked
 function goClicked() {
-    alert('You clicked the div!');
+    alert('You searched for a book');
+}
+
+function createClicked(){
+    alert('you added a book');
 }
 
 let go = document.getElementById('go');
-
 go.addEventListener('click', goClicked);
+
+let create = document.getElementById('create');
+create.addEventListener('click', createClicked);
+
