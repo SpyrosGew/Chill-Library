0195f38a0a5bb78360d235cf16cd16aec8feeef1
spyrosgew <spyrosgewdark@gmail.com>
2024-05-09 02:29:40 +0300

    stared Div Spawner

--- script.js
+++ script.js
@@ -7,9 +7,38 @@ function createClicked(){
     alert('you added a book');
 }
 
+function dataDivSpawner(title, author, price, genre) {
+    for (let i = 0; i < 3; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.className = "newDiv"; // Set the class name
+
+        let titleElem = document.createElement("div");
+        titleElem.textContent = "Title: " + title;
+
+        let authorElem = document.createElement("div");
+        authorElem.textContent = "Author: " + author;
+
+        let priceElem = document.createElement("div");
+        priceElem.textContent = "Price: " + price;
+
+        let genreElem = document.createElement("div");
+        genreElem.textContent = "Genre: " + genre;
+
+        newDiv.appendChild(titleElem);
+        newDiv.appendChild(authorElem);
+        newDiv.appendChild(priceElem);
+        newDiv.appendChild(genreElem);
+
+        var place = document.getElementById("results");
+        place.appendChild(newDiv);
+    }
+}
+
+
 let go = document.getElementById('go');
 go.addEventListener('click', goClicked);
 
 let create = document.getElementById('create');
 create.addEventListener('click', createClicked);
 
+dataDivSpawner();
